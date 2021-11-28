a3fa2ee59d70bb119384970f18449a6d89e7b344
JOY DEEP SAHA <88198830+sahajoydeep2002@users.noreply.github.com>
2021-11-28 16:55:37 +0530

    Delete src directory

--- src/constants/index.js
+++
@@ -1,9 +0,0 @@
-import { AppConfig } from 'blockstack';
-
-export const appConfig = new AppConfig(['store_write', 'publish_data']);
-
-export const PROFILE_FILENAME = 'profile.json';
-
-export const VOLUNTEERING_FILENAME = 'volunteering.json';
-
-export const EXPLORER_URL = 'https://explorer.blockstack.org';
